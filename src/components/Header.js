cdc68cdabd86f7d64f90eb86fd0fd58627705ea4
Corey Burkett <coreyburkett22@gmail.com>
2020-10-21 15:48:50 -0700

    Started react app and created header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <header className="App-header">
+        <h1 className="App-title display-4">Employee Directory</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
